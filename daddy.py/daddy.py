4472ca50b64c14fb3a868074510086c07a5768a0
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-05-22 17:30:01 +0500

    daddy

--- daddy.py/daddy.py
+++ daddy.py/daddy.py
@@ -1,4 +1,107 @@
-print('qalay eeee')
-print('assd')
-print("liboy nersa")
+from ast import MatchSequence
+from re import A
+from socket import IPV6_UNICAST_HOPS
+from ssl import SSL_ERROR_INVALID_ERROR_CODE
+
+
+# print('qalay eeee')
+# print('assd')
+# print("liboy nersa")
 # new line
+
+# yosh = int(input('Yoshingiz nechida? '))
+# if yosh<=4:
+#     price = 0
+# elif yosh<=12:
+#     price = 5000
+# else:
+#     price = 10000
+    
+# print(f"Sizga kirish {price} so'm")    
+# son = int(input('juft son kiriting '))
+# if son%2==0:
+#     print("to'gri") 
+# else:
+#     print('XATO!!!')    
+# Foydalanuvchi yoshini so'rang, va muzeyga kirish uchun chipta narhini quyidagicha chiqaring:
+
+# Agar foydalanuvchi 4 yoshdan kichkina yoki 60 dan katta bo'lsa bepul
+# Agar foydalanuvchi 18 dan kichik bo'lsa 10000 so'm
+# Agar foydalanuvchi 18 dan katta bo'lsa 20000 so'm
+# YOSH = int(input('yoshingiz nechchida '))
+# if YOSH <=4 or 60:
+#     print('sizga chiptamiz bepul !!! ')
+# elif YOSH <=18:
+#     print("sizga kirish 10000 so'm ")
+# b=  float(input("istalgan son kiriting "))
+# a = float(input("yana bitta "))
+# if a==b:
+#     print(f"{a}={b}")
+# elif a>=b:
+#     print(f"{a} > {b}") 
+# elif a<=b:
+#     print(f"{b}<{a}")  
+# mahsulot = ['banan','kivi','apelsin','tuxum','non'] 
+# savat = []
+# for a in range(5):
+#     savat.append(input(f"{a+1}-mahsulotni qo'shing!"))
+#     for mahsulotlar in savat:
+#         if mahsulotlar in mahsulot:
+#             print("bizda bu mahsulotlar bor")
+#         else:
+#             print("bizda bu mahsulot yo'q")    
+
+
+# son = int(input("Juft son kiriting: "))
+# if son%2==0:
+#     print("Bu son juft emas.")
+# else:
+#     print("Rahmat!")
+# yosh = int(input("yoshingiz nechchida "))
+# if yosh<=4 or yosh >=60:
+#     print("sizga chipta  bepul")
+# elif yosh <= 18:
+#     print('sizga chipta narxi 10000')
+# else:
+#     print("sizga chipta narxi 20000")
+# x = float(input("Birinchi sonni kiriting: "))
+# y = float(input("Ikkinchi sonni kiriting: "))
+# if x==y:
+#     print(f"{x}={y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# else:
+#     print(f"{x}>{y}")
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+              #  'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+# savat = []
+# for n in range(5):
+#    savat.append(input("5ta mahsulot kiriting ! "))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+
+# if mavjud_emas:
+#   print("Do'konimizda quyidagi mahsulotlar yo'q:")
+# for mahsulot in mavjud_emas:
+#   print(mahsulot)
+# for mahsulot in bor_mahsulotlar:
+#     print(mahsulot)
+daddy = ['alisher1983','aziza','yasina' 'umar']
+
+login = input("Yangi login tanlang:" )
+
+if login in daddy:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
+
+
+    
